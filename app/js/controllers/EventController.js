736bdc09700979f88523c9aa704cb80a3c68ea09
carmudgeon <diego.agudelo2@gmail.com>
2014-03-27 16:53:53 -0500

    [DHAM] Se estiliza el index.html

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,8 +3,20 @@ var eventControllers = angular.module('eventsApp.controllers',[]);
 
 eventControllers.controller('EventController',function($scope){
 	$scope.events = [
-		{name:"Angular Boot Camp part 1",date:"1/1/2014",time:'10:30 am'},
-		{name:"Angular Boot Camp part 2",date:"1/1/2014",time:'11:30 am'},
-		{name:"Angular Boot Camp part 3",date:"1/1/2014",time:'12:30 am'}
+		{name:"Angular Boot Camp part 1",date:"1/1/2014",time:'10:30 am',location:{
+			address:'Google Headquarters',
+			city:'Mountain View',
+			province:'CA'
+		}},
+		{name:"Angular Boot Camp part 2",date:"1/1/2014",time:'11:30 am',location:{
+			address:'Google Headquarters',
+			city:'Mountain View',
+			province:'CA'
+		}},
+		{name:"Angular Boot Camp part 3",date:"1/1/2014",time:'12:30 am',location:{
+			address:'Google Headquarters',
+			city:'Mountain View',
+			province:'CA'
+		}}
 	];
 });
