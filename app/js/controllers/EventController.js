47319e2f9d4de0d7a74265d49e89d097e005eb41
carmudgeon <diego.agudelo2@gmail.com>
2014-03-28 14:27:13 -0500

    [DHAM] Se adiciona la carpeta img y se notifica el cambio de version de Bootstrap

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,20 +3,107 @@ var eventControllers = angular.module('eventsApp.controllers',[]);
 
 eventControllers.controller('EventController',function($scope){
 	$scope.events = [
-		{name:"Angular Boot Camp part 1",date:"1/1/2014",time:'10:30 am',location:{
-			address:'Google Headquarters',
-			city:'Mountain View',
-			province:'CA'
-		}},
-		{name:"Angular Boot Camp part 2",date:"1/1/2014",time:'11:30 am',location:{
-			address:'Google Headquarters',
-			city:'Mountain View',
-			province:'CA'
-		}},
-		{name:"Angular Boot Camp part 3",date:"1/1/2014",time:'12:30 am',location:{
-			address:'Google Headquarters',
-			city:'Mountain View',
-			province:'CA'
-		}}
+		{name:"Angular Boot Camp part 1",date:"1/1/2014",time:'10:30 am',
+			imageUrl:'img/AngularJS-large.png',
+			location:{
+				address:'Google Headquarters',
+				city:'Mountain View',
+				province:'CA'
+			},
+			sessions:[
+				{
+					name:'Directives Masterclass1',
+					creatorName:'Bob Smith',
+					duration:'1 hr',
+					level:'Advanced',
+					abstract:'In this session you will learn the ins and outs of directives',
+					upVoteCount:0
+				},
+				{
+					name:'Scopes for fun and profit1',
+					creatorName:'John Doe',
+					duration:'30 mins',
+					level:'Introductory',
+					abstract:'This session will take a closer look at scopes',
+					upVoteCount:0
+				},
+				{
+					name:'Well Behaved Controllers1',
+					creatorName:'Jane Doe',
+					duration:'2 Hours',
+					level:'Intermediate',
+					abstract:'Controllers are the beginning of everything angular does...',
+					upVoteCount:0
+				}
+			]
+		},
+		{name:"Angular Boot Camp part 2",date:"1/1/2014",time:'11:30 am',
+			imageUrl:'img/AngularJS-large.png',
+			location:{
+				address:'Google Headquarters',
+				city:'Mountain View',
+				province:'CA'
+			},
+			sessions:[
+				{
+					name:'Directives Masterclass2',
+					creatorName:'Bob Smith',
+					duration:'1 hr',
+					level:'Advanced',
+					abstract:'In this session you will learn the ins and outs of directives',
+					upVoteCount:0
+				},
+				{
+					name:'Scopes for fun and profit2',
+					creatorName:'John Doe',
+					duration:'30 mins',
+					level:'Introductory',
+					abstract:'This session will take a closer look at scopes',
+					upVoteCount:0
+				},
+				{
+					name:'Well Behaved Controllers2',
+					creatorName:'Jane Doe',
+					duration:'2 Hours',
+					level:'Intermediate',
+					abstract:'Controllers are the beginning of everything angular does...',
+					upVoteCount:0
+				}
+			]
+		},
+		{name:"Angular Boot Camp part 3",date:"1/1/2014",time:'12:30 am',
+			imageUrl:'img/AngularJS-large.png',
+			location:{
+				address:'Google Headquarters',
+				city:'Mountain View',
+				province:'CA'
+			},
+			sessions:[
+				{
+					name:'Directives Masterclass3',
+					creatorName:'Bob Smith',
+					duration:'1 hr',
+					level:'Advanced',
+					abstract:'In this session you will learn the ins and outs of directives',
+					upVoteCount:0
+				},
+				{
+					name:'Scopes for fun and profit3',
+					creatorName:'John Doe',
+					duration:'30 mins',
+					level:'Introductory',
+					abstract:'This session will take a closer look at scopes',
+					upVoteCount:0
+				},
+				{
+					name:'Well Behaved Controllers3',
+					creatorName:'Jane Doe',
+					duration:'2 Hours',
+					level:'Intermediate',
+					abstract:'Controllers are the beginning of everything angular does...',
+					upVoteCount:0
+				}
+			]
+		},
 	];
 });
